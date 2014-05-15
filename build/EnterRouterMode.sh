bd0fa3f7e743247cdeb7024f2eaea59375d53749
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-05-15 15:58:04 -0700

    Restart udev once scripts have been updated Forces udev rules to be re-read.

--- build/EnterRouterMode.sh
+++ build/EnterRouterMode.sh
@@ -117,3 +117,8 @@ EOF
 #Persist configuration changes
 /usr/sbin/etc_tools p
 
+#restart udev
+
+killall udevd
+killall udevtrigger
+/etc/init.d/udev
--- footer.sh
+++ footer.sh
@@ -1,3 +1,8 @@
 #Persist configuration changes
 /usr/sbin/etc_tools p
 
+#restart udev
+
+killall udevd
+killall udevtrigger
+/etc/init.d/udev
