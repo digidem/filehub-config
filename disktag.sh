ba0ff96a219ca47dd3893e9266afec049c881158
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-05-19 10:35:39 -0700

    Update USB disk detection for USB hub

--- disktag.sh
+++ disktag.sh
@@ -10,6 +10,10 @@ usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.5 UsbDisk 6
 usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.6 UsbDisk 7
 usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.7 UsbDisk 8
 usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.8 UsbDisk 9
+usb1/1-1/1-1.2/1-1.2.1 UsbDisk 2
+usb1/1-1/1-1.2/1-1.2.2 UsbDisk 3
+usb1/1-1/1-1.2/1-1.2.3 UsbDisk 4
+usb1/1-1/1-1.2/1-1.2.4 UsbDisk 5
 usb1/1-1/1-1.1 UsbDisk 1
 usb1/1-1/1-1.2 UsbDisk 2
 usb1/1-1/1-1.3 UsbDisk 3
