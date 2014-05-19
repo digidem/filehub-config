66c8c045e45044056e93d066a698bc1db2fad307
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-05-19 01:34:14 -0700

    Add additional entries to /etc/init.d/disktag to support multiple drives attached to a USB hub

---
+++ disktag.sh
@@ -0,0 +1,19 @@
+
+# Updates /etc/init.d/disktag which determines the names of disks attached via USB
+
+cat  <<'EOF' > /etc/init.d/disktag
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.1 UsbDisk 2
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.2 UsbDisk 3
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.3 UsbDisk 4
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.4 UsbDisk 5
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.5 UsbDisk 6
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.6 UsbDisk 7
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.7 UsbDisk 8
+usb1/1-1/1-1.2/1-1.2.1/1-1.2.1.8 UsbDisk 9
+usb1/1-1/1-1.1 UsbDisk 1
+usb1/1-1/1-1.2 UsbDisk 2
+usb1/1-1/1-1.3 UsbDisk 3
+usb2/2-1/2-1.1 UsbDisk 1
+usb2/2-1/2-1.2 UsbDisk 2
+usb2/2-1/2-1.3 UsbDisk 3
+EOF
