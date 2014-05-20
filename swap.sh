3d7bc9b4efe933e3461e2336d227c6efd399d4bc
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-05-20 12:38:57 -0700

    Add logging to swap file creation

--- swap.sh
+++ swap.sh
@@ -4,15 +4,23 @@
 cat <<'EOF' > /etc/init.d/swap
 STORE_DIR=/monitoreo
 CONFIG_DIR="$STORE_DIR"/no_tocar
+rm -f /tmp/swapinfo
 
 while read device mountpoint fstype remainder; do
     if [ ${device:0:7} == "/dev/sd" -a -e "$mountpoint$CONFIG_DIR" ];then
-            local swapfile
             swapfile="$mountpoint$CONFIG_DIR"/swapfile
             if [ ! -e "$swapfile" ]; then
                 dd if=/dev/zero of="$swapfile" bs=1024 count=65536
+                echo "Creating swapfile $swapfile" >> /tmp/swapinfo
             fi
-            swapon "$swapfile"
+            swapon "$swapfile" >> /tmp/swapinfo 2>&1
+            if [ $? -eq 0 ]; then
+                echo "Turned on swap for $swapfile" >> /tmp/swapinfo
+            else
+                echo "There was an error turning on swap" >> /tmp/swapinfo
+            fi
+            exit 0
     fi
 done < /proc/mounts
+exit 0
 EOF
